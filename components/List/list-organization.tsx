a8b538512bcb8b99d644009f5378410cf95331e9
Florian Winkler <flo@wnklr.art>
2022-05-09 21:49:51 +0200

    Update support page

--- components/List/list-organization.tsx
+++ components/List/list-organization.tsx
@@ -1,38 +1,29 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import { Row, Col } from 'reactstrap'
-import { Container } from '../Layout'
-import list from './styles.module.scss'
+import Link from 'next/link';
+import Image from 'next/image';
+import { Row, Col } from 'reactstrap';
+import { Container } from '../Layout';
+import list from './styles.module.scss';
 
 interface Props {
-  title?: string,
-  description?: string,
-  link?: string,
+	title?: string;
+	description?: string;
+	link?: string;
 }
 
-export const ListOrganization:  React.FunctionComponent<Props> = ({
-  title,
-  description,
-  link
+export const ListOrganization: React.FunctionComponent<Props> = ({
+	title,
+	description,
+	link,
 }) => {
-  return (
-      <div className={ list['list--organization'] } >
-        <h2 className={ list['list--organization__title'] } >
-          { title }
-        </h2>
-        <p className={ list['list--organization__description'] } >
-          { description }
-        </p>
-        <div className={ list['list--organization__ctas'] }>
-          <Link href={ link || '' }>
-            <a>
-              Go to website
-            </a>
-          </Link>
-          <div className={ list['list--organization__donate'] } >
-            Make donation
-          </div>
-        </div>
-      </div>
-  )
-}
+	return (
+		<div className={list['list--organization']}>
+			<h2 className={list['list--organization__title']}>{title}</h2>
+			<p className={list['list--organization__description']}>{description}</p>
+			<div className={list['list--organization__ctas']}>
+				<Link href={link || ''}>
+					<a>Go to website</a>
+				</Link>
+			</div>
+		</div>
+	);
+};
--- pages/support.tsx
+++ pages/support.tsx
@@ -22,25 +22,25 @@ const Support: NextPage = () => {
 				<Section>
 					<Container>
 						<Row>
-							<Col md={6}>
+							<Col md={8}>
 								<p>
 									Being a white male working in the tech industry I have
-									benefited from a privileges in my life. Including having a
+									benefited from a privileges in my life. Including a
 									financially secure family background that has given me the
 									freedom to not have to worry about money. Having that
-									background, it is very important to me to support people and
-									organizations and try to do good with the things I have and
-									can do. With that being said I have listed some organizations
-									that I regularly support with small donations.
+									background, it is very important to me to support
+									organizations that do good. And to think about ways to do good
+									within the spectrum of my work. Below you will find
+									organizations that do great work.
 								</p>
 							</Col>
-							<Col md={6}>
+							<Col md={8}>
 								<p>
-									I am currently about to think about ways to implement features
-									in my software and design that people can pay for and will be
-									100% donated to a beneficial organization. If you have ideas
-									and thoughts around that topic, I would be more than curious
-									to exchange them with you.
+									Currently I am thinking about ways to implement features in
+									software and design that people can pay for (if they want)
+									that will be 100% donated to a beneficial organization. If you
+									have ideas and thoughts around that topic, I would be more
+									than curious to exchange ideas with you.
 								</p>
 							</Col>
 						</Row>
@@ -49,32 +49,25 @@ const Support: NextPage = () => {
 				<Section>
 					<Container>
 						<Row>
-							<Col md={12}>
+							<Col md={6}>
 								<ListOrganization
 									title='Bildungsinitiative Ferhatunvar'
-									description='Serpil möchte mit der Gründung der Bildungsinitiative allen Kindern, Jugendlichen, jungen Erwachsenen und deren Eltern, die rassistische Erfahrungen im Alltag oder in der Schule machen, eine Anlaufstelle bieten. Mit dieser Arbeit möchte Serpil das Gedenken an ihren Sohn Ferhat aufrechterhalten. Die Bildungsinitiative wird von der Familie Unvar, Jugendlichen, engagierten Erwachsenen, sowie Freund:innen von Ferhat getragen.'
+									description='Lorem ipsum dolores and so one and on'
 									link='https://www.bildungsinitiative-ferhatunvar.de/'
 								/>
 							</Col>
-							<Col md={12}>
+							<Col md={6}>
 								<ListOrganization
 									title='Sea Watch'
 									description='Lorem ipsum dolores and so one and on'
 									link='#'
 								/>
 							</Col>
-							<Col md={12}>
+							<Col md={6}>
 								<ListOrganization
 									title='Deutsche Depressionshilfe'
 									description='Lorem ipsum dolores and so one and on'
 									link='https://www.deutsche-depressionshilfe.de/start'
-								/>
-							</Col>
-							<Col md={12}>
-								<ListOrganization
-									title='Weißer Ring'
-									description='Lorem ipsum dolores and so one and on'
-									link='#'
 								/>
 							</Col>
 						</Row>
