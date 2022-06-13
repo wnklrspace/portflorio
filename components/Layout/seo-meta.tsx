12cd0f30b4b47213afe90e8e1ba19a9dc8ce9ad8
Florian Winkler <flo@wnklr.art>
2022-06-13 14:46:55 +0200

    Add facivons

--- components/Layout/seo-meta.tsx
+++ components/Layout/seo-meta.tsx
@@ -1,114 +1,71 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 interface Props {
-  title?: string,
-  desc?: string,
-  urlPath?: string,
-  socialImage?: string,
+	title?: string;
+	desc?: string;
+	urlPath?: string;
+	socialImage?: string;
 }
 
 export const SeoMeta: React.FunctionComponent<Props> = (props) => {
+	const { title, desc, urlPath, socialImage } = props;
 
-  const {
-    title,
-    desc,
-    urlPath,
-    socialImage
-  } = props;
+	const mainUrl = 'https://portflorio.com/';
+	const mainTitle = 'Florian Winkler ';
+	const author = 'Florian Winkler';
 
-  const mainUrl = 'https://portflorio.com/';
-  const mainTitle = 'Florian Winkler ';
-  const author = 'Florian Winkler';
+	return (
+		<Head>
+			<title>
+				{mainTitle}
+				{title}
+			</title>
+			<meta name='description' content={desc} />
+			<link rel='canonical' href={`${mainUrl}${urlPath}`} />
+			<meta charSet='utf-8' />
+			<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+			<meta name='author' content={author} />
+			<link
+				rel='apple-touch-icon'
+				sizes='180x180'
+				href='/apple-touch-icon.png'
+			/>
+			<link
+				rel='icon'
+				type='image/png'
+				sizes='32x32'
+				href='/favicon-32x32.png'
+			/>
+			<link
+				rel='icon'
+				type='image/png'
+				sizes='16x16'
+				href='/favicon-16x16.png'
+			/>
+			<link rel='manifest' href='/site.webmanifest' />
+			<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+			<meta name='msapplication-TileColor' content='#da532c' />
+			<meta name='theme-color' content='#ffffff' />
 
-  return (
-    <>
-    <Head>
-      <title>{ mainTitle }{ title }</title>
-      <meta
-        name='description'
-        content={ desc }
-      />
-      <link
-        rel='canonical'
-        href={ `${ mainUrl }${ urlPath }`  }
-      />
-      <meta charSet='utf-8' />
-      <meta
-        name='viewport'
-        content='initial-scale=1.0, width=device-width'
-      />
-      <meta
-        name='author'
-        content={ author }
-      />
-      <link
-        rel='icon'
-        href='/favicon.ico'
-      />
-      <link
-        rel='icon'
-        type='image/png'
-        href='/favicon.ico'
-      />
-      <link
-        rel='apple-touch-icon'
-        href='/favicon.ico'
-      />
+			{/* og */}
+			<meta property='og:type' content='website' />
+			<meta
+				name='og:title'
+				property='og:title'
+				content={`${mainTitle}${title}`}
+			/>
+			<meta name='og:description' property='og:description' content={desc} />
+			<meta property='og:site_name' content='' />
+			<meta property='og:url' content={`${mainUrl}${urlPath}`} />
+			<meta property='og:image' content={socialImage} />
 
-      {/* og */}
-      <meta
-        property='og:type'
-        content='website'
-      />
-      <meta
-        name='og:title'
-        property='og:title'
-        content={ `${ mainTitle }${ title }`  }
-      />
-      <meta
-        name='og:description'
-        property='og:description'
-        content={ desc }
-      />
-      <meta
-        property='og:site_name'
-        content=''
-      />
-      <meta
-        property='og:url'
-        content={ `${ mainUrl }${ urlPath }` }
-      />
-      <meta
-        property='og:image'
-        content={ socialImage }
-      />
-
-      {/* twitter */}
-      <meta
-        name='twitter:card'
-        content='summary'
-      />
-      <meta
-        name='twitter:title'
-        content={ `${ mainTitle }${ title }`  }
-      />
-      <meta
-        name='twitter:description'
-        content={ desc }
-      />
-      <meta
-        name='twitter:site'
-        content=''
-      />
-      <meta
-        name='twitter:creator'
-        content={ author }
-      />
-      <meta
-        name='twitter:image'
-        content={ socialImage }
-      />
-    </Head>
-    </>
-  )
-}
+			{/* twitter */}
+			<meta name='twitter:card' content='summary' />
+			<meta name='twitter:title' content={`${mainTitle}${title}`} />
+			<meta name='twitter:description' content={desc} />
+			<meta name='twitter:site' content='' />
+			<meta name='twitter:creator' content={author} />
+			<meta name='twitter:image' content={socialImage} />
+		</Head>
+	);
+};
